d538c978a850b8a25dc7e71b840ab37be7419320
rtyawegn <yannik.wegner@stud.hs-ruhrwest.de>
2022-05-09 09:49:18 +0200

    new test

--- src/de/hrw/progra2/assignment5/Intern.java
+++ src/de/hrw/progra2/assignment5/Intern.java
@@ -2,13 +2,13 @@ package de.hrw.progra2.assignment5;
 
 public class Intern {
 
-    public boolean validLabourHours(){
+    /*public boolean validLabourHours(){
         float counter = 0.0f;
 
         for (float labourHour : labourHours) {
             counter += labourHour;
-        } //kurzer test test testest
-    }
+        }
+    }*/
 
 
 }
